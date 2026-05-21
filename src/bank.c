fc8648d33f41c4d9f7e70a9d2407061cf0ad9b30
senqui <hcquindao@up.edu.ph>
2026-05-22 01:24:31 +0800

    [Feat] Implement balance, deposit, withdraw, and transfer functions

---
+++ src/bank.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+#include "bank.h"
+#include "lock_mgr.h"
+
+Bank bank;
+
+int get_balance(int account_id){
+    Account *acc = &bank.accounts[account_id];
+    pthread_rwlock_rdlock(&acc->lock);
+    int balance = acc->balance_centavos;
+    pthread_rwlock_unlock(&acc->lock);
+    return balance;
+}
+
+void deposit(int account_id, int amount){
+    Account *acc = &bank.accounts[account_id];
+    pthread_rwlock_wrlock(&acc->lock);
+    acc->balance_centavos += amount;
+    pthread_rwlock_unlock(&acc->lock);
+}
+
+bool withdraw(int account_id,int amount){
+    Account *acc = &bank.accounts[account_id];
+    pthread_rwlock_wrlock(&acc->lock);
+    if (acc->balance_centavos < amount) {
+        pthread_rwlock_unlock(&acc->lock);
+        return false;
+    }
+    acc->balance_centavos -= amount;
+    pthread_rwlock_unlock(&acc->lock);
+    return true;
+}
+
+bool transfer(int tx_id, int from_id, int to_id, int amount)
+{
+    Account *from = &bank.accounts[from_id];
+    Account *to = &bank.accounts[to_id];
+    pthread_rwlock_wrlock(&from->lock);
+    from->lock_owner = tx_id;
+    if (pthread_rwlock_trywrlock(&to->lock) != 0) {
+        record_wait(tx_id,
+                    to_id,
+                    to->lock_owner);
+        if (detect_deadlock()) {
+            printf("[DEADLOCK DETECTED] Transaction %d aborted\n", tx_id);
+            pthread_rwlock_unlock(&from->lock);
+            clear_wait(tx_id);
+            return false;
+        }
+        pthread_rwlock_wrlock(&to->lock);
+    }
+
+    to->lock_owner = tx_id;
+
+    if (from->balance_centavos < amount) {
+        pthread_rwlock_unlock(&to->lock);
+        pthread_rwlock_unlock(&from->lock);
+        clear_wait(tx_id);
+        return false;
+    }
+
+    from->balance_centavos -= amount;
+    to->balance_centavos += amount;
+    pthread_rwlock_unlock(&to->lock);
+    pthread_rwlock_unlock(&from->lock);
+    clear_wait(tx_id);
+    return true;
+}
