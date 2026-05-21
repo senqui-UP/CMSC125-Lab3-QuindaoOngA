b0b849f87f50afdaba985e0026a36c8077af71f4
senqui <hcquindao@up.edu.ph>
2026-05-22 00:43:05 +0800

    [Feat] Implement metrics

--- src/metrics.c
+++ src/metrics.c
@@ -1,3 +1,22 @@
-//
-// Created by ANDY DOMINIC ONG on 5/21/26.
-//
+#include <stdio.h>
+
+#include "metrics.h"
+#include "timer.h"
+
+Metrics metrics;
+
+void print_metrics()
+{
+    printf("\n=== Summary ===\n");
+    printf("Total transactions : %d\n",
+           metrics.total_transactions);
+
+    printf("Committed : %d\n",
+           metrics.committed);
+
+    printf("Aborted : %d\n",
+           metrics.aborted);
+
+    printf("Total ticks : %d\n",
+           global_tick);
+}
